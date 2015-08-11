9a8d6edeed93514f53c05ec0b579f7740f9aff06
stefanocudini <stefano.cudini@gmail.com>
2015-08-11 17:54:24 +0200

    added masonry

--- client/views/panels/places.js
+++ client/views/panels/places.js
@@ -1,4 +1,11 @@
-
+/*
+Template.panelPlaces.onRendered(function() {
+	this.$('.panel-body').masonry({
+	  columnWidth: 100,
+	  itemSelector: '.place-box'
+	});
+});
+*/
 Template.panelPlaces.events({	
 	'mouseenter .well': function(e, tmpl) {
 		e.preventDefault();
