b0bdb399b77a92ca9dbe37e81cea4008baa2c06a
stefanocudini <stefano.cudini@gmail.com>
2016-05-27 23:18:52 +0200

    streetview

--- server/streetview.js
+++ server/streetview.js
@@ -17,7 +17,7 @@
 Meteor.methods({
 	getStreetViewById: function(placeId) {
 		
-		console.log("getStreeViewById()",placeId);
+		console.log("getStreeViewById()",placeId, Googlemaps);
 
 		var url = "http://maps.googleapis.com/maps/api/streetview?"+
 		 		"size=200x200&location={loc}&sensor=false";
@@ -25,44 +25,16 @@ Meteor.methods({
 		var place = getPlaceById(placeId).fetch()[0],
 			lastP = place.loc;
 
-		if(place.tracks>0) {
+		/*if(place.tracks>0) {
 			var track = getTracksByIds(place.tracks).fetch()[0],
 				firstP = track.features[0].geometry.coordinates[0].reverse();
-		}
+		}*/
 		//TODO calcolare angolo e inclinazione da fisrtP a lastP
 
-// // http://code.google.com/apis/maps/documentation/streetview
-// exports.streetView = function(size, location, callback, sensor, heading, fov, pitch) {
+		//// http://code.google.com/apis/maps/documentation/streetview
+		//size, location, callback, sensor, heading, fov, pitch
+		var res = Googlemaps.streetView('250x200', lastP.join(','), false);
 
-
-		return Googlemaps.streetView('250x200', lastP.join(','),false);
-
-		// ll = K.util.geo.roundLoc(ll, 8);
-		// var key = parseInt(K.util.timeUnix()/(60*60*24*1))+'_'+ll.join('_');	//daily hash
-		// var val = K.cache.get(key ,'meteo');
-		// return val || K.cache.set(key, meteoAPI(ll) ,'meteo');
-/*
-function computeAngle(endLatLng, startLatLng) {
-      var DEGREE_PER_RADIAN = 57.2957795;
-      var RADIAN_PER_DEGREE = 0.017453;
- 
-      var dlat = endLatLng.lat() - startLatLng.lat();
-      var dlng = endLatLng.lng() - startLatLng.lng();
-      // We multiply dlng with cos(endLat), since the two points are very closeby,
-      // so we assume their cos values are approximately equal.
-      var yaw = Math.atan2(dlng * Math.cos(endLatLng.lat() * RADIAN_PER_DEGREE), dlat)
-             * DEGREE_PER_RADIAN;
-      return wrapAngle(yaw);
-   }
- 
-   function wrapAngle(angle) {
-    if (angle >= 360) {
-        angle -= 360;
-    } else if (angle < 0) {
-        angle += 360;
-    }
-    return angle;
-    };
-*/
+		return res;
 	}
 });
