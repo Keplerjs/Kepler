7fd6ff82c558077126f5a28e6d099e41c1a7f26f
stefanocudini <stefano.cudini@gmail.com>
2017-07-07 13:53:08 -0100

    plugin events work in progress

---
+++ packages/keplerjs-events/package.js
@@ -0,0 +1,31 @@
+
+Package.describe({
+  name: 'keplerjs:events',
+  summary: 'keplerjs plugin events',
+  version: '0.0.1',
+  git: ''
+});
+
+Package.onUse(function(api) {
+
+  api.versionsFrom("METEOR@1.0");
+  
+  api.use([
+    'keplerjs:core'
+  ]);
+
+  api.addFiles([
+    'plugin.js',    
+    //'collections/events.js'
+  ]);
+
+/*  api.addFiles([
+    'client/views/panels.html',
+    'client/router.js'    
+  ],'client');*/
+
+/*  api.addFiles([
+    'server/pubs.js',
+  ],'server');*/
+  
+});
---
+++ packages/keplerjs-events/plugin.js
@@ -0,0 +1,19 @@
+
+K.Plugin({
+	name: 'events',
+	placeholders: {
+		panelPlace: 'panelPlace_events'
+	},
+	filters: {
+		placePanel: {
+			fields: {
+				events: 1
+			}
+		},
+		userPanel: {
+			fields: {
+				events: 1
+			}
+		}		
+	}
+});
