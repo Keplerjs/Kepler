bf59d947c6cb19bef0cf528b72d514c1dd4d244d
stefanocudini <stefano.cudini@gmail.com>
2017-09-01 01:29:25 -0100

    rank in points

--- packages/keplerjs-export/server/places.js
+++ packages/keplerjs-export/server/places.js
@@ -7,7 +7,10 @@ WebApp.connectHandlers.use('/export/places',function(req, res, next) {
 	var data = Places.find({}, K.filters.placeItem).fetch();
 
 	var features = _.map(data, function(d) {
-		return K.Util.geo.createFeature('Point', K.Util.geo.roundLoc(d.loc, 2).reverse() )
+		return K.Util.geo.createFeature('Point',
+			K.Util.geo.roundLoc(d.loc, 2).reverse(), {
+				rank: (d.rank+1) * (d.checkins.length+1) * (d.convers.length+1)
+			});
 	});
 
 	data = K.Util.geo.createFeatureColl(features);
