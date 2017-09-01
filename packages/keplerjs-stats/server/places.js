c065533d2c50c34d1e65ac50f5e6c98853fe31c4
stefanocudini <stefano.cudini@gmail.com>
2017-09-01 15:32:20 -0100

    plugin stats support direct ajax call, best rank of users stats

--- packages/keplerjs-stats/server/places.js
+++ packages/keplerjs-stats/server/places.js
@@ -9,18 +9,30 @@ WebApp.connectHandlers.use('/stats/places',function(req, res, next) {
 		'Content-type': 'application/json'
 	});
 	
-	var data = Places.find({}, K.filters.placeItem).fetch();
+	var data = Places.find({}, {
+		fields: { createdAt:1, loc:1, rank:1, checkins:1, hist:1, convers:1 },
+		sort: { createdAt: -1},
+		//TODO limit
+	}).fetch();
 
 	var features = _.map(data, function(d) {
-		return K.Util.geo.createFeature('Point',
-			K.Util.geo.roundLoc(d.loc, 2).reverse(), {
-				rank: (d.rank+1) * (d.checkins.length+1) * (d.convers.length+1)
+		var loc = K.Util.geo.roundLoc(d.loc, 2);
+		return K.Util.geo.createFeature('Point', loc.reverse(), {
+				rank: (d.rank+1) * 
+					  (d.checkins && d.checkins.length+1) * 
+					  (d.convers && d.convers.length+1) *
+					  (d.hist && d.hist.length+1)
 			});
 	});
 
+	//TODO compute percentage of rank
+
 	data = K.Util.geo.createFeatureColl(features);
 
-	var jsonp = (req.query && req.query.jsonp) || 'jsonpcall';
+	if(req.query && req.query['jsonp'])
+		data = req.query['jsonp']+'('+JSON.stringify(data)+');';
+	else
+		data = JSON.stringify(data);
 
-	res.end(jsonp+'('+JSON.stringify(data)+');');
+	res.end(data);
 });
--- packages/keplerjs-stats/server/users.js
+++ packages/keplerjs-stats/server/users.js
@@ -1,4 +1,6 @@
-
+/*
+	//TODO move it in plugin api
+*/
 WebApp.connectHandlers.use('/stats/users',function(req, res, next) {
 
 	//console.log('Stats: ', req.originalUrl)
@@ -8,22 +10,30 @@ WebApp.connectHandlers.use('/stats/users',function(req, res, next) {
 	});
 	
 	var data = Users.find({}, {
-		fields: {loc:1, loclast:1, places:1, friends:1, convers:1, hist:1}
+		fields: { createdAt:1, loc:1, loclast:1, places:1, friends:1, convers:1, hist:1, favorites:1 },
+		sort: { createdAt: -1},
+		//TODO limit
 	}).fetch();
 
 	var features = _.map(data, function(d) {
-		var loc = K.Util.geo.roundLoc(d.loc || d.loclast, 2) || [];
+		var loc = K.Util.geo.roundLoc(d.loc || d.loclast, 1) || [];
 		return K.Util.geo.createFeature('Point', loc.reverse(), {
-				rank: (d.places && d.places.length+1) * 
+				rank: (d.favorites && d.favorites.length+1) * 
 					  (d.friends && d.friends.length+1) * 
 					  (d.convers && d.convers.length+1) *
+					  (d.places && d.places.length+1) * 
 					  (d.hist && d.hist.length+1)
 			});
 	});
 
+	//TODO compute percentage of rank
+
 	data = K.Util.geo.createFeatureColl(features);
 
-	var jsonp = (req.query && req.query.jsonp) || 'jsonpcall';
+	if(req.query && req.query['jsonp'])
+		data = req.query['jsonp']+'('+JSON.stringify(data)+');';
+	else
+		data = JSON.stringify(data);
 
-	res.end(jsonp+'('+JSON.stringify(data)+');');
+	res.end(data);
 });
