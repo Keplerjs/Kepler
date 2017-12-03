e73228e1685eca1b8e8666933ab23ea9d062954d
stefanocudini <stefano.cudini@gmail.com>
2017-12-03 18:05:17 +0100

    smtp in start script

--- private/start.sh
+++ private/start.sh
@@ -14,6 +14,7 @@ PORT="127.0.0.1:3000"
 
 MYIP=$( hostname -i | cut -f1 -d' ')
 export MONGO_URL="mongodb://localhost:27017/$DBNAME"
+export MAIL_URL="smtp://localhost:25"
 
 if [ $MYIP = '127.0.1.1' ]; then
 	echo "START DEVELOPMENT MODE"
