45fb979a6a1409b6a17ea28c11b1c041bd164905
stefanocudini <stefano.cudini@gmail.com>
2018-04-02 21:30:26 +0200

    unpublish scripot

---
+++ private/unpublish_packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash  
+
+meteor admin set-unmigrated  keplerjs:base
+meteor admin set-unmigrated  keplerjs:theme
+meteor admin set-unmigrated  keplerjs:api
+meteor admin set-unmigrated  keplerjs:edit
+meteor admin set-unmigrated  keplerjs:admin
+meteor admin set-unmigrated  keplerjs:stats
+meteor admin set-unmigrated  keplerjs:upload
+meteor admin set-unmigrated  keplerjs:share
+meteor admin set-unmigrated  keplerjs:geoinfo
+meteor admin set-unmigrated  keplerjs:osm
+meteor admin set-unmigrated  keplerjs:favorites
+meteor admin set-unmigrated  keplerjs:convers
+meteor admin set-unmigrated  keplerjs:notifs
+meteor admin set-unmigrated  keplerjs:pois
+meteor admin set-unmigrated  keplerjs:tracks
+meteor admin set-unmigrated  keplerjs:routing
