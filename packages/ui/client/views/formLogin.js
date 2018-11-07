cf773e24088df7e3e5c60bcc02a7d38403e843bf
Stefano Cudini <stefano.cudini@gmail.com>
2018-11-07 17:46:43 +0100

    add foligin

---
+++ packages/ui/client/views/formLogin.js
@@ -0,0 +1,18 @@
+
+Template.formLogin.helpers({
+	classnames: function() {
+		var sets = K.settings.public.accounts,
+			serv = sets.services,
+			c = '';
+		if(!sets.creation)
+			c += ' no-accounts-creation';
+		else {
+			if(!serv.facebook) c += ' no-accounts-facebook';
+			if(!serv.google) c += ' no-accounts-google';
+			if(!serv.github) c += ' no-accounts-github';
+			if(!serv.twitter) c += ' no-accounts-twitter';
+			if(!serv.openstreetmap) c += ' no-accounts-openstreetmap';
+		}
+		return c;
+	}
+})
