4d82b36ccf655bf4d7c4a6c65b8743b93ba36017
stefanocudini <stefano.cudini@gmail.com>
2018-12-12 17:51:45 +0100

    heroku script

---
+++ private/deploy_heroku.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#this script deploy keplerjs app in heroku servers
+
+#https://medium.com/@leonardykris/how-to-run-a-meteor-js-application-on-heroku-in-10-steps-7aceb12de234
+
+#and a mongodb database in mlab.com
