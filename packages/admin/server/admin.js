49f17edf3accfc0e46ffcda1c62d2ff156b8be93
Stefano Cudini <stefano.cudini@gmail.com>
2019-01-23 16:33:23 +0100

    plugin admin auto update adminUsers at startup

--- packages/admin/server/admin.js
+++ packages/admin/server/admin.js
@@ -11,25 +11,47 @@ Meteor.methods({
 });
 
 
-Accounts.onLogin(function(login) {
+/*Accounts.onLogin(function(login) {
 
-	if(login.user && login.user._id)
+	if(login.user && login.user._id && K.Admin.isMe(login.user)) {
+
+		console.log('Admin: update adminUsers...', K.settings.admin.adminUsers);
+
 		Users.update(login.user._id, {
 			$set: {
-				isAdmin: K.Admin.isMe(login.user) ? 1 : 0
+				isAdmin: 1
 			}
 		});
-});
-
-
-/*
-//TODO
-Meteor.startup(function() {	
-	if(!Users.findOne({username: 'admin'})) {
-		console.log('Admin: autocreate admin account user: admin pass: adminadmin ');
-		//TODO	
 	}
-});*/
+});
+*/
+Meteor.startup(function() {
+	if(K.settings.admin.adminUsers.length) {
+
+		var on = Users.update({
+			username: {$in: K.settings.admin.adminUsers}
+		}, {
+			$set: {
+				isAdmin: 1
+			}
+		},{ multi: true });
+
+		var of = Users.update({
+			username: {$nin: K.settings.admin.adminUsers}
+		}, {
+			$set: {
+				isAdmin: 0
+			}
+		},{ multi: true });
+
+		console.log('Admin: update adminUsers...', K.settings.admin.adminUsers);
+
+		//TODO	
+		/*if(!Users.findOne({username: 'admin'})) {
+			console.log('Admin: autocreate admin account user: admin pass: adminadmin ');
+		}*/
+	}
+});
 
 /*
 RESET admin password, uncomment for use
