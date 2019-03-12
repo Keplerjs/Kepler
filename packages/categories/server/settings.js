6887676c915e7d51d9e9c44f56b445383d237316
stefanocudini <stefano.cudini@gmail.com>
2019-03-12 14:37:05 +0100

    plugin categories checks settinfa

--- packages/categories/server/settings.js
+++ packages/categories/server/settings.js
@@ -5,43 +5,45 @@ Meteor.startup(function() {
 	var sets = K.settings.public.categories.cats,
 		cats = [];
 
-	//TODO Cate
-	console.log('Cats: load categories from settings...');
+	if(sets && sets.place && sets.user) {
 
-	_.each(sets.place, function(active, name) {
-		if(active) {
-			cats.push(_.extend({}, K.schemas.cat, {
-				name: name,
-				active: active,
-				type: 'place'
-			}));
-		}
-	});
+		_.each(sets.place, function(active, name) {
+			if(active) {
+				cats.push(_.extend({}, K.schemas.cat, {
+					name: name,
+					active: active,
+					type: 'place'
+				}));
+			}
+		});
 
-	_.each(sets.user, function(active, name) {
-		if(active) {
-			cats.push(_.extend({}, K.schemas.cat, {
-				name: name,
-				active: active,
-				type: 'user'
-			}));
-		}
-	});
+		_.each(sets.user, function(active, name) {
+			if(active) {
+				cats.push(_.extend({}, K.schemas.cat, {
+					name: name,
+					active: active,
+					type: 'user'
+				}));
+			}
+		});
 
-	_.each(cats, function(cat) {
+		_.each(cats, function(cat) {
 
-		try {
-			var ret = Categories.upsert({
-				name: cat.name,
-				type: cat.type
-			}, {
-				$set: cat
-			});
+			try {
+				var ret = Categories.upsert({
+					name: cat.name,
+					type: cat.type
+				}, {
+					$set: cat
+				});
 
-		} catch(e) {
-			//throw new Meteor.Error(500, i18n('error_cats_exists',name) );
-			console.log( i18n('error_cats_exists',cat.name), 'type', cat.type );
-			return null;
-		}
-	});
+			} catch(e) {
+				//throw new Meteor.Error(500, i18n('error_cats_exists',name) );
+				console.log( i18n('error_cats_exists',cat.name), 'type', cat.type );
+				return null;
+			}
+		});
+
+		console.log('Cats: load categories from settings', cats.length);
+	}
 });
