64661d94b9c16ab3ff83eadb247452e74f8a83bb
stefanocudini <stefano.cudini@gmail.com>
2019-06-18 22:21:46 +0200

    fix draw handlers css

--- packages/stats/client/Map_stats.js
+++ packages/stats/client/Map_stats.js
@@ -3,29 +3,30 @@ Tracker.autorun(function(comp) {
 
 	if(K.Map.ready()) {
 
-		if(!K.Map.layers.stats)
-		K.Map.layers.stats = L.geoJSON([], {
-			pointToLayer: function(point, loc) {
-				var r = point.properties.rank;
-				r = Math.min(r, 15);
-				r = Math.max(r, 5);
-				return L.circleMarker(loc, {radius: r }).on('click', function(e) {
-					L.DomEvent.stopPropagation(e);
-					K.Map.map.removeLayer(K.Map.layers.stats);
-					K.Map.map.flyTo(loc, K.settings.public.map.dataMinZoom+1, {
-						duration: 1
-					})
-				});
-			},
-			style: K.settings.public.stats.mapStyle
-		});
+		if(!K.Map.layers.stats) {
+			K.Map.layers.stats = L.geoJSON([], {
+				pointToLayer: function(point, loc) {
+					var r = point.properties.rank;
+					r = Math.min(r, 15);
+					r = Math.max(r, 5);
+					return L.circleMarker(loc, {radius: r }).on('click', function(e) {
+						L.DomEvent.stopPropagation(e);
+						K.Map.map.removeLayer(K.Map.layers.stats);
+						K.Map.map.flyTo(loc, K.settings.public.map.dataMinZoom+1, {
+							duration: 1
+						})
+					});
+				},
+				style: K.settings.public.stats.mapStyle
+			});
+		}
 
 		K.Map.map.on('zoomend', function(e) {
 			
 			var bbox = K.Map.getBBox(),
 				z = K.Map.map.getZoom();
 
-//L.rectangle(bbox,{style:{opacity:.25}}).addTo(K.Map.map)
+			//L.rectangle(bbox,{style:{opacity:.25}}).addTo(K.Map.map)
 
 			if(z < K.settings.public.map.dataMinZoom) {
 				
@@ -34,7 +35,7 @@ Tracker.autorun(function(comp) {
 				if(K.Map.layers.stats.getLayers().length===0) {
 					Meteor.call('findStatsPlacesByGeo', bbox, function(err, geojson) {
 
-//console.log(bbox.toString(), geojson.features.length)
+						//console.log(bbox.toString(), geojson.features.length)
 						K.Map.layers.stats.clearLayers();
 						K.Map.layers.stats.addData(geojson);
 					});		
