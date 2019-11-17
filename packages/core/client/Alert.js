ab2175b34c2e8ecb74ae92b54cfd6f2c18d685c5
stefanocudini <stefano.cudini@gmail.com>
2019-11-17 21:15:32 +0100

    comments

--- packages/core/client/Alert.js
+++ packages/core/client/Alert.js
@@ -1,4 +1,11 @@
-
-Kepler.Alert = sAlert;
-
-//TODO implements more methods
+/**
+ * show pretty alerts on top page
+ * @param  {String} text shown
+ */
+Kepler.Alert = _.extend(sAlert, {
+	//extend this lib https://github.com/juliancwirko/meteor-s-alert
+	//TODO show message only one time
+	/*once: function(text, type) {
+		console.log('once time!')
+	}*/
+});
