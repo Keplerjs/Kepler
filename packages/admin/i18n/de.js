7bd1b1862dd951463f6784e37db8ea1d2f44d44e
weidi <jweidacher@gmail.com>
2021-09-06 21:53:37 +0200

    Update de.js

--- packages/admin/i18n/de.js
+++ packages/admin/i18n/de.js
@@ -2,5 +2,20 @@
 i18n.map('de', {
 	title_admin: "Admin",
 
-	label_rawdata: "Rohdaten"
+	title_adminUser: "Admin Nutzer: {$1}",
+	title_adminPlace: "Admin Ort: {$1}",
+	
+	title_panelAdminUsers: "User bearbeiten",
+	title_panelAdminPlaces: "Orte bearbeiten",
+	title_panelAdminMethods: "Admin Funktionen",
+
+	label_rawdata: "Rohdaten",
+	label_logins: "Sessions",
+	label_nologged: "Not logged",
+	label_ip: "IP Adresse",
+
+	btn_userdel: "Nutzer löschen!",
+	btn_addfriends: "Freund hinzufügen",
+
+	error_admin_rawedit: "Rohdaten fehlerhaft"
 });
